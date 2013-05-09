f9bc059b987843bd232ede1e165eb7767cb1019f
Mathieu Gagné <mathieu@motioneleven.com>
2013-05-09 17:39:59 -0400

    Add month & categories to index

--- app/models/kennedy/post.rb
+++ app/models/kennedy/post.rb
@@ -4,5 +4,16 @@ module Kennedy
     attr_accessible :category_ids
     has_and_belongs_to_many :categories
 
+    searchable do
+      string :month, stored: true
+      string :categories, multiple: true, stored: true do
+        categories.map(&:name)
+      end
+    end
+
+    def month
+      @month ||= published_at.strftime('%B %Y')
+    end
+
   end
 end
