e240d4c929a2fc67f64b5e8641a7678c51fb61fa
Mathieu Gagné <mathieu@motioneleven.com>
2013-05-15 15:33:57 -0400

    Add recent scope

--- app/models/kennedy/post.rb
+++ app/models/kennedy/post.rb
@@ -1,19 +1,14 @@
 module Kennedy
   class Post < Georgia::Page
 
-    attr_accessible :category_ids
     has_and_belongs_to_many :categories
 
-    searchable do
-      string :month, stored: true
-      string :categories, multiple: true, stored: true do
-        categories.map(&:name)
-      end
-    end
+    scope :recent, order(:published_at)
 
     def month
       @month ||= published_at.strftime('%B %Y')
     end
 
   end
+
 end
