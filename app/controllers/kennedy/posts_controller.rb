1255dd584505c75380ddc696ae8e11bf7adcc99a
Mathieu Gagné <mathieu@motioneleven.com>
2013-05-15 15:37:50 -0400

    Add extra search params in search block

--- app/controllers/kennedy/posts_controller.rb
+++ app/controllers/kennedy/posts_controller.rb
@@ -3,8 +3,20 @@ module Kennedy
 
     include Georgia::Concerns::Pageable
     include Georgia::Concerns::Publishable
+    include Georgia::Concerns::Searchable
 
     load_and_authorize_resource class: Kennedy::Post
+
+    private
+
+    # Adds to search block called from Concerns::Searchable
+    def extra_search_params
+      Proc.new {
+        facet :month, :categories
+        with(:month, params[:m]) unless params[:m].blank?
+        with(:categories, params[:c]) unless params[:t].blank?
+      }
+    end
 
   end
 
