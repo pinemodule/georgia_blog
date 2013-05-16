9413bc3bf2e1fe57841fd6812e7c36b021be0d4f
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-05-16 10:36:47 -0400

    fixed bug with overidden methods

--- app/helpers/kennedy/application_helper.rb
+++ app/helpers/kennedy/application_helper.rb
@@ -1,18 +1,10 @@
 module Kennedy
   module ApplicationHelper
 
-    def render_template template, record
-      begin
-        render "Kennedy/posts/templates/#{template}", template: template, record: record
-      rescue
-        render "kennedy/posts/templates/custom", template: template, record: record
-      end
-    end
-
     def link_to_preview(post, options={})
       options[:method] = :post
       options[:target] = '_blank'
-      link_to "#{icon_tag('icon-eye-open')} Preview".html_safe, preview_post_path(post, post: post.attributes.merge(contents: post.contents.map(&:attributes))), options
+      link_to "#{icon_tag('icon-eye-open')} Preview".html_safe, main_app.preview_post_path(post, post: post.attributes.merge(contents: post.contents.map(&:attributes))), options
     end
 
   end
