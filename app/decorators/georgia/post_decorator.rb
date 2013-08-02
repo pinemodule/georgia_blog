60c280ef3f2ca944240e514eff68cf3b46a9bc02
Mathieu Gagné <mathieu@motioneleven.com>
2013-08-02 17:27:59 -0400

    Remove all legacy category-related methods in PostDecorator

--- app/decorators/georgia/post_decorator.rb
+++ app/decorators/georgia/post_decorator.rb
@@ -1,30 +1,4 @@
 module Georgia
   class PostDecorator < Georgia::PageDecorator
-
-    def template_path
-      "posts/templates/#{model.template}"
-    end
-
-    def url options={}
-      localized_slug(options) + blog_slug + categorized_url
-    end
-
-    def localized_slug options={}
-      if options[:locale].present?
-        "/#{options[:locale]}/"
-      else
-        (I18n.available_locales.length > 1) ? "/#{I18n.locale.to_s}/" : '/'
-      end
-    end
-
-    def categorized_url options={}
-      category = options[:category] || source.categories.first
-      [category.try(:slug), source.slug].compact.join('/')
-    end
-
-    def blog_slug
-      'blog/'
-    end
-
   end
 end
