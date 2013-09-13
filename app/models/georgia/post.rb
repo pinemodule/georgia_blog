a171973b452e3a122f87f876830b1353d203b769
Mathieu Gagné <mathieu@motioneleven.com>
2013-09-13 14:03:20 -0400

    Move indexed_fields to real searchable block

--- app/models/georgia/post.rb
+++ app/models/georgia/post.rb
@@ -17,7 +17,45 @@ module Georgia
     end
 
     searchable do
-      instance_eval &Georgia::Post.indexable_fields
+      text :title, stored: true do
+        revisions.map{|r| r.contents.map(&:title)}.flatten.uniq.join(', ')
+      end
+      text :excerpt, stored: true do
+        revisions.map{|r| r.contents.map(&:excerpt)}.flatten.uniq.join(', ')
+      end
+      text :text do
+        revisions.map{|r| r.contents.map(&:text)}.flatten.uniq.join(', ')
+      end
+      text :keywords do
+        revisions.map{|r| r.contents.map(&:keyword_list)}.flatten.uniq.join(', ')
+      end
+      text :tags do
+        tag_list.join(', ')
+      end
+      text :url
+      text :template
+      string :title
+      string :excerpt
+      string :text
+      string :url
+      string :template
+      string :state do
+        public? ? 'public' : 'private'
+      end
+      string :class_name do
+        self.class.name
+      end
+      string :keywords, stored: true, multiple: true do
+        revisions.map{|r| r.contents.map(&:keyword_list)}.flatten.uniq
+      end
+      string :tag_list, stored: true, multiple: true # Array for facets
+      string :tags, stored: true do # Single list for ordering
+        tag_list.join(', ')
+      end
+      string :month, stored: true
+      string :year, stored: true
+      time :published_at
+      time :updated_at # default order
     end
 
     class << self
@@ -30,49 +68,6 @@ module Georgia
         }
       end
 
-      def indexable_fields
-        Proc.new {
-          text :title, stored: true do
-            revisions.map{|r| r.contents.map(&:title)}.flatten.uniq.join(', ')
-          end
-          text :excerpt, stored: true do
-            revisions.map{|r| r.contents.map(&:excerpt)}.flatten.uniq.join(', ')
-          end
-          text :text do
-            revisions.map{|r| r.contents.map(&:text)}.flatten.uniq.join(', ')
-          end
-          text :keywords do
-            revisions.map{|r| r.contents.map(&:keyword_list)}.flatten.uniq.join(', ')
-          end
-          text :tags do
-            tag_list.join(', ')
-          end
-          text :url
-          text :template
-          string :title
-          string :excerpt
-          string :text
-          string :url
-          string :template
-          string :state do
-            public? ? 'public' : 'private'
-          end
-          string :class_name do
-            self.class.name
-          end
-          string :keywords, stored: true, multiple: true do
-            revisions.map{|r| r.contents.map(&:keyword_list)}.flatten.uniq
-          end
-          string :tag_list, stored: true, multiple: true # Array for facets
-          string :tags, stored: true do # Single list for ordering
-            tag_list.join(', ')
-          end
-          string :month, stored: true
-          string :year, stored: true
-          time :published_at
-          time :updated_at # default order
-        }
-      end
     end
   end
 end
