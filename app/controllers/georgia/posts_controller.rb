5e84c8d27f8b161d73b0bbaac5863ff4735d5a96
Mathieu Gagné <mathieu@motioneleven.com>
2014-02-06 14:00:54 -0500

    Fail-safe prepare_post_data

--- app/controllers/georgia/posts_controller.rb
+++ app/controllers/georgia/posts_controller.rb
@@ -1,12 +1,12 @@
 module Georgia
   class PostsController < Georgia::PagesController
 
-    before_filter :prepare_post_data
+    before_filter :prepare_post_data, only: [:update, :settings]
 
     private
 
     def prepare_post_data
-      @post_data ||= @page.build_post_data unless @page.post_data
+      @post_data ||= @page.build_post_data unless @page and @page.post_data
     end
   end
 end
