f78021c0c6e247ad6eedec8a0da9cbe95e2929e0
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-07-21 14:18:27 -0400

    Fixes category list on posts#update

--- app/controllers/georgia/blog/posts_controller.rb
+++ app/controllers/georgia/blog/posts_controller.rb
@@ -10,9 +10,20 @@ module Georgia
         @post_data ||= @page.build_post_data unless @page and @page.post_data
       end
 
-      def permitted_extra_params
-        [{ category_list: [] }, { post_data_attributes: [:published_at, :author_id] }]
+      def sanitized_params
+        parsed_params = ParseJsonTags.new(post_params).call
+        parsed_params = ParseJsonTags.new(parsed_params, key: :category_list).call
+        parsed_params
       end
+
+      def post_params
+        params.require(:page).permit(permitted_keys + extra_permitted_keys)
+      end
+
+      def extra_permitted_keys
+        [ :category_list, { post_data_attributes: [:published_at, :author_id] } ]
+      end
+
     end
   end
 end
